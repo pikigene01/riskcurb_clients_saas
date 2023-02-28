8ef14d7894eac38ac7728067245a582a1e745be6
Gene Piki <pikigene01@gmail.com>
2023-02-28 04:26:09 +0200

    update

--- create.php
+++ create.php
@@ -1,43 +1,108 @@
-<?php 
-include_once('includes/cors.php');
-include_once('includes/host.php');
-if(isset($_POST['site_name'])){
-$site_name = $_POST['site_name'];
-$user_email = $_POST['email'];
-$user_password = $_POST['password'];//bycrypted password sent
-$con = new mysqli($host,$user,$password);
+<?php
 
-$create = $con->query("CREATE DATABASE $site_name");
+include('includes/cors.php');
+include('includes/host.php');
 
-if($create){
-   //load sql file
+$message = "error creating new database";
+exit(json_encode(
+    array(
+        'status' => 400,
+        'message' => $_POST
 
-    $con_2 = new mysqli($host,$user,$password,$site_name);
-    $time = new DateTime();
-    $user_token = "qwertyuioadsghzvgdfuypbmcbjhcba$time";
-    //insert values bycrypted password
-    $save_user = $con_2->query("INSERT INTO users (email,name,password,role,created_at) VALUES ('$user_email','$site_name','$user_password','admin',NOW() ) ");
-    $save_user_token = $con_2->query("INSERT INTO tokens (email,token,created_at) VALUES ('$user_email','$user_token',NOW() ) ");
+    )
 
-     if($save_user){
-    exit(json_encode(
-        array(
-            'status'=>200,
-            'message'=>'Database created successfully',
-            'token'=>$user_token
-            )
-        
-    ));
-}else{
-    exit(json_encode(
-        array(
-            'status'=>200,
-            'message'=>'there was an error occurred'
-            
-            )
-        
-    ));
-}
-}
-    
+));
+if (isset($_POST['site_name'])) {
+ 
+    $site_name = $_POST['site_name'];
+    $user_email = $_POST['email'];
+    $user_password = $_POST['password']; //bycrypted password sent
+    $con = new mysqli($host, $user, $password);
+
+    $create = $con->query("CREATE DATABASE $site_name");
+
+    if ($create) {
+        //load sql file
+
+        if ($create) {
+            try {
+
+                // Temporary variable, used to store current query
+                $templine = '';
+                // Read in entire file
+                $filename = __DIR__ . '/includes/sql/saas_sql.sql';
+                $lines = file($filename);
+                //  Loop through each line
+                foreach ($lines as $line) {
+                    $startWith = substr(trim($line), 0, 2);
+                    $endWith = substr(trim($line), -1, 1);
+                    // Skip it if it's a comment
+                    if (empty($line) || $startWith == '--' || $startWith == '/*' || $startWith == '//')
+
+                        continue;
+
+                    // Add this line to the current segment
+                    $templine .= $line;
+                    // If it has a semicolon at the end, it's the end of the query
+                    if (substr(trim($line), -1, 1) == ';') {
+                        // Perform the query
+                        $conn_2 = mysqli_connect($host, $user, $password, $site_name);
+
+                        $sql = $conn_2->query($templine) or print('Error performing query \'<strong>' . $templine . '\': <br /><br />');
+                        // Reset temp variable to empty
+                        $templine = '';
+
+
+                        $con_2 = new mysqli($host, $user, $password, $site_name);
+                        $time = new DateTime();
+                        $user_token = "qwertyuioadsghzvgdfuypbmcbjhcba$time";
+                        $token = str_shuffle($user_token);
+                        $token = substr($user_token, 4, 12);
+                        //insert values bycrypted password
+                        $save_user = $con_2->query("INSERT INTO users (email,name,password,role,token,created_at) VALUES ('$user_email','$site_name','$user_password','__admin','$token',NOW() ) ");
+                    
+                        if ($save_user) {
+                            exit(json_encode(
+                                array(
+                                    'status' => 200,
+                                    'message' => 'Database created successfully',
+                                    'token' => $token
+                                )
+
+                            ));
+                        } else {
+                            exit(json_encode(
+                                array(
+                                    'status' => 200,
+                                    'message' => 'there was an error occurred'
+
+                                )
+
+                            ));
+                        }
+                    }
+                }
+            } catch (Exception $e) {
+                
+                exit(json_encode(
+                    array(
+                        'status' => 400,
+                        'message' =>  $e->getMessage()
+
+                    )
+
+                ));
+            }
+        } else {
+            $message = "error creating new database";
+            exit(json_encode(
+                array(
+                    'status' => 400,
+                    'message' => $message
+
+                )
+
+            ));
+        }
+    }
 }
--- includes/cors.php
+++ includes/cors.php
@@ -1,7 +1,7 @@
 <?php
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers:  *");
+header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods:  POST, PUT, GET');
 
 ?>
